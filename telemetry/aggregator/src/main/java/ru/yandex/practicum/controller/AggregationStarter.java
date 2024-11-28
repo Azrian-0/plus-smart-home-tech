5646cc28ec2fe70e05f48396635dc865fd3d6185
Azrian <noman89reg@gmail.com>
2024-11-29 00:15:07 +0300

    Исправление по комментариям.

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/controller/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/controller/AggregationStarter.java
@@ -6,6 +6,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.KafkaConfig;
@@ -34,31 +35,38 @@ public class AggregationStarter {
                 if (records.isEmpty()) continue;
 
                 for (ConsumerRecord<String, SensorEventAvro> record : records) {
-                    log.info("Получено сообщение: {}, со смещением {}", record.value(), record.offset());
+                    try {
+                        log.info("Получено сообщение: {}, со смещением {}", record.value(), record.offset());
 
-                    Optional<SensorsSnapshotAvro> snapshotAvro = snapshotRepository.updateState(record.value());
-                    if (snapshotAvro.isPresent()) {
-                        ProducerRecord<String, SpecificRecordBase> producerRecord =
-                                new ProducerRecord<>(config.getTopicOut(), snapshotAvro.get().getHubId(),
-                                        snapshotAvro.get());
-                        producer.send(producerRecord, (recordMetadata, e) -> {
-                            if (e != null) {
-                                log.error("Ошибка отправки", e);
-                                throw new RuntimeException("Ошибка отправки", e);
-                            }
-                            log.info("Сообщение отправлено в topic - {}", recordMetadata.topic());
-                        });
+                        Optional<SensorsSnapshotAvro> snapshotAvro = snapshotRepository.updateState(record.value());
+                        if (snapshotAvro.isPresent()) {
+                            ProducerRecord<String, SpecificRecordBase> producerRecord =
+                                    new ProducerRecord<>(config.getTopicOut(), snapshotAvro.get().getHubId(),
+                                            snapshotAvro.get());
+                            producer.send(producerRecord, (recordMetadata, e) -> {
+                                if (e != null) {
+                                    log.error("Ошибка отправки", e);
+                                    throw new RuntimeException("Ошибка отправки", e);
+                                }
+                                log.info("Сообщение отправлено в topic - {}", recordMetadata.topic());
+                            });
+                        }
+
+                        consumer.commitSync(Collections.singletonMap(
+                                new TopicPartition(record.topic(), record.partition()),
+                                new OffsetAndMetadata(record.offset() + 1)
+                        ));
+                    } catch (Exception e) {
+                        log.error("Ошибка обработки записи: {}, смещение {}", record.value(), record.offset(), e);
                     }
                 }
-                consumer.commitSync();
             }
         } catch (WakeupException ignored) {
-            // игнорируем - закрываем консьюмер и продюсер в блоке finally
         } catch (Exception e) {
             log.error("Ошибка во время обработки событий от датчиков", e);
         } finally {
             log.info("Закрываем консьюмер");
-            consumer.wakeup();
+            consumer.close();
             log.info("Закрываем продюсер");
             producer.close();
         }
